848109ade66ddc0cf567aebc53bfec5750bdd8fe
nathan_priandi <nathan.priandi@gmail.com>
2026-01-01 01:31:42 +0700

    Fix API URL

--- constants/Api.js
+++ constants/Api.js
@@ -1,7 +1,5 @@
 const API_BASE_URL = __DEV__
   ? 'http://192.168.1.21:8000'
-  : 'https://pycourse-backend.koyeb.app';
-
-  const PROD_BACKEND = 'https://monetary-lynna-nathanpriandi-be7bcb98.koyeb.app/';
+  : 'https://monetary-lynna-nathanpriandi-be7bcb98.koyeb.app';
 
 export default API_BASE_URL;
